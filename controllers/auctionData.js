5d669d30209ba75b4cbff5ac691916481e57e8e7
Antti Romppanen <romppanen.antti@gmail.com>
2022-07-19 11:44:29 +0300

    uibuild & heroku region change

--- controllers/auctionData.js
+++ controllers/auctionData.js
@@ -5,6 +5,7 @@ const getApiKey = require('../utils/getApiKey');
 
 auctionDataRouter.get('/', async (req, res) => {
   const key = await getApiKey(config.CLIENT_ID, config.CLIENT_SECRET);
+
   const auctionData = await axios({
     url: 'https://eu.api.blizzard.com/data/wow/connected-realm/4476/auctions/6?namespace=dynamic-classic-eu&locale=en_US',
     method: 'get',
